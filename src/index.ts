361e70bb18844ec41815900283d1bb8a4fa304e9
Abhishek Raj <abhi@raj.me>
2021-10-06 17:00:25 +0530

    Add support for utils api

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import * as fs from './fs';
 import * as aws from './aws';
+import * as utils from './common/utils';
 
 // Ref: https://stackoverflow.com/a/41283945
-export { fs, aws }; // named exports
-export default { fs, aws }; // default export
+export { fs, aws, utils }; // named exports
+export default { fs, aws, utils }; // default export
