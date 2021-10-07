8e657a2142149ffa30a831696ccf1ed155b4b8f1
Abhishek Raj <abhi@raj.me>
2021-10-07 20:12:59 +0530

    Remove utils api from this package

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,7 @@
 import * as fs from './fs';
 import * as aws from './aws';
 import * as gh from './gh';
-import * as utils from './common/utils';
 
 // Ref: https://stackoverflow.com/a/41283945
-export { fs, aws, gh, utils }; // named exports
-export default { fs, aws, gh, utils }; // default export
+export { fs, aws, gh }; // named exports
+export default { fs, aws, gh }; // default export
