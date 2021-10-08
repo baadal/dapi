f115edd7e64eac7dc952345a06a07a7a941e0dc2
Abhishek Raj <abhi@raj.me>
2021-10-08 11:29:26 +0530

    Auto-init aws on load

--- src/aws/index.ts
+++ src/aws/index.ts
@@ -1,15 +1,19 @@
 import * as dbAll from './db';
 import { error } from '../common/logger';
 
-const { init: dbInit, ...db } = dbAll;
+const { init: dbInit, status: dbStatus, ...db } = dbAll;
 
 const init = (region: string) => {
   if (!region) {
     error(`AWS initialization error! Missing region: ${region}`);
-    return;
+    return false;
   }
 
-  dbInit(region);
+  return dbInit(region);
 };
 
-export { db, init }; // named exports
+const status = () => {
+  return dbStatus();
+};
+
+export { db, init, status }; // named exports
