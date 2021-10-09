e4bdf0e992a75f843abaa1526f228541c89d36ea
Abhishek Raj <abhi@raj.me>
2021-10-09 18:47:52 +0530

    Add aws.s3 api

--- src/aws/index.ts
+++ src/aws/index.ts
@@ -1,10 +1,12 @@
 import * as db from './db';
+import * as s3 from './s3';
 import { warn, error } from '../common/logger';
 
 // Ref: https://docs.aws.amazon.com/AWSJavaScriptSDK/v3/latest/index.html
 // Types: https://docs.aws.amazon.com/AWSJavaScriptSDK/v3/latest/modules/_aws_sdk_types.html
 
 const { init: dbInit, status: dbStatus } = db;
+const { init: s3Init, status: s3Status } = s3;
 
 /**
  * @deprecated explicit init deprecated!
@@ -16,13 +18,16 @@ export const init = (region: string) => {
     return false;
   }
 
-  return dbInit(region);
+  const cond1 = dbInit(region);
+  const cond2 = s3Init(region);
+  return cond1 && cond2;
 };
 
 export const status = () => {
   return {
     db: dbStatus(),
+    s3: s3Status(),
   };
 };
 
-export { db }; // named exports
+export { db, s3 }; // named exports
