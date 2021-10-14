cbdf1eed67833949927106f625ccfbab70295702
Abhishek Raj <abhi@raj.me>
2021-10-14 10:57:58 +0530

    Add node utils api

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,8 @@
 import * as fs from './fs';
 import * as aws from './aws';
 import * as gh from './gh';
+import * as utils from './utils';
 
 // Ref: https://stackoverflow.com/a/41283945
-export { fs, aws, gh }; // named exports
-export default { fs, aws, gh }; // default export
+export { fs, aws, gh, utils }; // named exports
+export default { fs, aws, gh, utils }; // default export
---
+++ src/utils/index.ts
@@ -0,0 +1,33 @@
+import crypto from 'crypto';
+import fsa from 'fs/promises';
+
+import { error } from '../common/logger';
+
+export const sha1Hash = (data: string) => {
+  if (!data) return null;
+  const hashSum = crypto.createHash('sha1');
+  hashSum.update(data);
+  return hashSum.digest('hex');
+};
+
+export const sha256Hash = (data: string | Buffer) => {
+  if (!data) return null;
+  const hashSum = crypto.createHash('sha256');
+  hashSum.update(data);
+  return hashSum.digest('hex');
+};
+
+export const fileHash = async (file: string) => {
+  if (!file) return null;
+  let contents: Buffer | null = null;
+
+  try {
+    // get buffer (instead of utf8 string) to support binary data
+    contents = await fsa.readFile(file);
+  } catch (e) {
+    error(e);
+    return null;
+  }
+
+  return sha256Hash(contents);
+};
