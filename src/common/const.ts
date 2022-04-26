16e41f170997c34be869cff6938c407751ef6eaa
Abhishek Raj <abhi@raj.me>
2022-04-27 02:26:05 +0530

    Add aws.db.writeItemUnique() func

--- src/common/const.ts
+++ src/common/const.ts
@@ -1,2 +1,3 @@
 export const BATCH_SIZE = 20; // (max) number of items in a batch request
 export const CHUNK_SIZE = 10; // (max) number of parallel requests at a time
+export const MAX_RETRY_ATTEMPTS = 3; // (max) number of retry attempts in case of key conflict
